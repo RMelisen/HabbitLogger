cec99e526a0d759ee20d5a184bcd1848ffaf2f26
Romain Mélisen <r.melisen@outlook.com>
2025-03-18 11:04:46 +0100

    Initialize repository + Create empty Sqlite DB

---
+++ HabbitLogger/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.Sqlite;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string connectionString = @"Data Source=habbitLogger.db";
+
+        using (SqliteConnection dbConnection = new SqliteConnection(connectionString))
+        {
+            dbConnection.Open();
+
+            SqliteCommand tableCommand = dbConnection.CreateCommand();
+            
+            tableCommand.CommandText = 
+                @"CREATE TABLE IF NOT EXISTS habbit (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Importance INTEGER)";
+            
+            tableCommand.ExecuteNonQuery();
+
+            dbConnection.Close();
+        }
+    }
+}
